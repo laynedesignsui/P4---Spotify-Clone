3e1d50cfa02fa1ed5312cbc270ff6d09836ec711
laynedesignsui <laynedesignsui@gmail.com>
2022-10-05 21:41:38 -0400

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,25 @@
+import DetailsHeader from "./DetailsHeader";
+import RelatedSongs from "./RelatedSongs";
+import MusicPlayer from "./MusicPlayer";
+import ArtistCard from "./ArtistCard";
+import Searchbar from "./Searchbar";
+import SongCard from "./SongCard";
+import TopPlay from "./TopPlay";
+import SongBar from "./SongBar";
+import Sidebar from "./Sidebar";
+import Loader from "./Loader";
+import Error from "./Error";
+
+export {
+  DetailsHeader,
+  RelatedSongs,
+  MusicPlayer,
+  ArtistCard,
+  Searchbar,
+  SongCard,
+  TopPlay,
+  Sidebar,
+  SongBar,
+  Loader,
+  Error,
+};
---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import ArtistDetails from "./ArtistDetails";
+import SongDetails from "./SongDetails";
+import TopArtists from "./TopArtists";
+import TopCharts from "./TopCharts";
+import AroundYou from "./AroundYou";
+import Discover from "./Discover";
+import Search from "./Search";
+
+export {
+  ArtistDetails,
+  SongDetails,
+  TopArtists,
+  TopCharts,
+  AroundYou,
+  Discover,
+  Search,
+};
